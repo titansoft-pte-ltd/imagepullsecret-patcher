3e18797326825c4da203b84742afb1e75e89bbbe
Matej Kern <matej.kern@infobip.com>
2021-03-26 16:41:45 +0100

    Added new informer in case secrets are manually updated or deleted

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -30,7 +31,8 @@ var (
 	configAllServiceAccount    bool          = false
 	configDockerconfigjson     string        = ""
 	configDockerConfigJSONPath string        = ""
-	configSecretName           string        = "image-pull-secret" // default to image-pull-secret
+	configSecretName           string        = "image-pull-secret"       // default to image-pull-secret
+	configSecretNamespace      string        = "imagepullsecret-patcher" // default to imagepullsecret-patcher
 	configExcludedNamespaces   string        = ""
 	configServiceAccounts      string        = defaultServiceAccountName
 	configUseInfromers         bool          = true
@@ -58,6 +60,7 @@ func main() {
 	flag.StringVar(&configDockerconfigjson, "dockerconfigjson", LookupEnvOrString("CONFIG_DOCKERCONFIGJSON", configDockerconfigjson), "json credential for authenicating container registry, exclusive with `dockerconfigjsonpath`")
 	flag.StringVar(&configDockerConfigJSONPath, "dockerconfigjsonpath", LookupEnvOrString("CONFIG_DOCKERCONFIGJSONPATH", configDockerConfigJSONPath), "path to json file containing credentials for the registry to be distributed, exclusive with `dockerconfigjson`")
 	flag.StringVar(&configSecretName, "secretname", LookupEnvOrString("CONFIG_SECRETNAME", configSecretName), "set name of managed secrets")
+	flag.StringVar(&configSecretNamespace, "secretnamespace", LookupEnvOrString("CONFIG_SECRET_NAMESPACE", configSecretNamespace), "namespace where original secret can be found")
 	flag.StringVar(&configExcludedNamespaces, "excluded-namespaces", LookupEnvOrString("CONFIG_EXCLUDED_NAMESPACES", configExcludedNamespaces), "comma-separated namespaces excluded from processing")
 	flag.StringVar(&configServiceAccounts, "serviceaccounts", LookupEnvOrString("CONFIG_SERVICEACCOUNTS", configServiceAccounts), "comma-separated list of serviceaccounts to patch")
 	flag.BoolVar(&configUseInfromers, "use-infromers", LookUpEnvOrBool("CONFIG_USE_INFROMERS", configUseInfromers), "if true, k8s informers to detect when new namespace is created and then it will run patching process, if false it runs in a loop for all namespaces")
@@ -130,11 +133,60 @@ func main() {
 
 func startNamespaceWatcher(k8s *k8sClient) {
 	factory := informers.NewSharedInformerFactory(k8s.clientset, 0)
-	informer := factory.Core().V1().Namespaces().Informer()
+	namespaceInformer := factory.Core().V1().Namespaces().Informer()
+	secretInformer := factory.Core().V1().Secrets().Informer()
 	stopper := make(chan struct{})
 	defer close(stopper)
 
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	secretInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			secret := obj.(*v1.Secret)
+			if secret.Name == configSecretName && secret.Namespace == configSecretNamespace {
+				log.Debugf("Original secret [%s] in namespace [%s]", secret.Name, secret.Namespace)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			secret := obj.(*v1.Secret)
+			if secret.Name == configSecretName && secret.Namespace != configSecretNamespace {
+				log.Debugf("Deleted secret [%s] in namespace [%s]", secret.Name, secret.Namespace)
+				var err error
+				namespace := secret.Namespace
+				namespaceObj, err := k8s.clientset.CoreV1().Namespaces().Get(namespace, metav1.GetOptions{})
+				if err != nil {
+					log.Panic(err)
+				}
+				if namespaceIsExcluded(*namespaceObj) {
+					log.Infof("[%s] Namespace skipped", namespaceObj.Name)
+				}
+				if namespaceObj.Status.Phase != "Terminating" {
+					log.Debugf("[%s] Start processing", namespace)
+					// for each namespace, make sure the dockerconfig secret exists
+					err = processSecret(k8s, namespace)
+					if err != nil {
+						// if has error in processing secret, should skip processing service account
+						log.Error(err)
+					}
+					// get default service account, and patch image pull secret if not exist
+					err = processServiceAccount(k8s, namespace)
+					if err != nil {
+						log.Error(err)
+					}
+				} else {
+					log.Debugf("[%s] namespace is in phase %s", namespace, namespaceObj.Status.Phase)
+				}
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			secret := oldObj.(*v1.Secret)
+			if secret.Name == configSecretName && secret.Namespace == configSecretNamespace {
+				log.Debugf("Updated secret [%s] in namespace [%s]", secret.Name, secret.Namespace)
+				log.Debug("Running update loop")
+				loop(k8s)
+			}
+		},
+	})
+
+	namespaceInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			var err error
 			ns := obj.(*corev1.Namespace)
@@ -155,7 +207,6 @@ func startNamespaceWatcher(k8s *k8sClient) {
 			if err != nil {
 				log.Error(err)
 			}
-
 		},
 		DeleteFunc: func(obj interface{}) {
 			ns := obj.(*corev1.Namespace)
@@ -163,7 +214,8 @@ func startNamespaceWatcher(k8s *k8sClient) {
 			log.Debugf("[%s] Namespace deleted", namespace)
 		},
 	})
-	informer.Run(stopper)
+	go namespaceInformer.Run(stopper)
+	secretInformer.Run(stopper)
 }
 
 func loop(k8s *k8sClient) {
